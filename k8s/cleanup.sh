0bdf42ce622e4170b5ae223aa7d8bd42fa5f0bc7
JamesTsui <tsuisauchi.tsc@gmail.com>
2022-10-22 12:50:10 +0800

    Added setup and cleanup script

---
+++ k8s/cleanup.sh
@@ -0,0 +1,4 @@
+kubectl delete -f ./ingress.yaml
+kubectl delete -f ./service.yaml
+kubectl delete -f ./database.yaml
+kubectl delete -f ./application.yaml
---
+++ k8s/setup.sh
@@ -0,0 +1,4 @@
+kubectl apply -f ./ingress.yaml
+kubectl apply -f ./service.yaml
+kubectl apply -f ./database.yaml
+kubectl apply -f ./application.yaml
