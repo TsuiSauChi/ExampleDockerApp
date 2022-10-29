532aa5e29904e16633376fd8b606de9179df548a
JamesTsui <tsuisauchi.tsc@gmail.com>
2022-10-29 17:19:55 +0800

    Switch from docker hub to ghcr registry; Switch to kustomize

--- k8s/cleanup.sh
+++
@@ -1,4 +0,0 @@
-kubectl delete -f ./ingress.yaml
-kubectl delete -f ./service.yaml
-kubectl delete -f ./database.yaml
-kubectl delete -f ./application.yaml
--- k8s/setup.sh
+++
@@ -1,4 +0,0 @@
-kubectl apply -f ./ingress.yaml
-kubectl apply -f ./service.yaml
-kubectl apply -f ./database.yaml
-kubectl apply -f ./application.yaml
